d801405c72303632ad59964c5c7514f388995738
Gustavo <andrek_96_10@hotmail.com>
2021-10-30 22:49:50 -0500

    codigo javascript añadido

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $('#boton').click(function(e){
         e.preventDefault();
+        let buscar = $('#buscar').val();
+        $('#h2').html(buscar);
         console.log('hello bitchess');
     })
 });
