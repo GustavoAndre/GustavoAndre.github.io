2fb5bd5df7703acfad8cfa4ef6c0e2d7ee9709f9
Gustavo <andrek_96_10@hotmail.com>
2021-10-30 23:44:08 -0500

    cambio 30/10/2021

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
     $('#boton').click(function(e){
         e.preventDefault();
-        let buscar = $('#buscar').val();
-        $('#h2').html(buscar);
-        console.log('hello bitchess');
+        let buscarn = $('#buscar').val();
+        
+         let template = `<h2> ${buscarn} </h2>`;
+
+         $('#cont').html(template);
+        console.log(buscarn);
     })
 });
